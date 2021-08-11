504f4f8ee659d2063c6b59bfe2fdc4349ff1a5aa
MohammadHasan <rony15987@yahoo.com>
2021-08-11 15:23:09 -0400

    Completed assignment

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+function Links({github, linkedin}){
+return(
+<>
+<h3>Links</h3>
+    {<a href= {github}>https://github.com/liza</a>}
+    {<a href= {linkedin}>https://www.linkedin.com/in/liza/</a>}
+</>
+
+)
+}
+export default Links
